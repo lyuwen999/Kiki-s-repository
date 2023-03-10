0421f864be1e78c93f0b666cfca8c70d14af3a46
Yiqing lyu <email@example.comgit config --global user.name Your>
2023-03-10 11:41:19 +0800

    learnpy1

---
+++ test.py
@@ -0,0 +1,95 @@
+#name=input('piease enter your name')
+#print('hello,',name)
+
+a=100
+if a>=0:
+    print(a)
+else:
+    print(-a)
+
+1000_1111
+1.23e10
+print(100/3)
+print("i\'m \"ok\"")
+print(r'\\\\\\\\')
+print('''line1
+line2
+ine3''')
+
+NAME=111
+10/3
+10//3
+10%3
+print(r'\\\\\\')
+ord('a')
+chr('200')
+'ABC'.encode('ascii')
+
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+'Hello,%s'%'world'
+#%d %f %s %x
+print('%2d-%02d'%(3,1))
+print('%.2f'%3.1415926)
+'growth rate:%d %%'
+'Hello,{0},成绩提升了{1:1f}%',format('xisoming',17.123)
+classmates=['Michael','Bob','Tracy']
+len(classmates)#3
+classmates[0]#'Michael'
+len(classmates)-1
+classmates[-1]
+classmates.append('Adam')
+classmates.insert(1,'JACK')
+classmates.pop()
+'Adam'
+classmates.pop(1)
+'jack'
+classmates[1]='Sarah'
+L=['Apple',123,True]
+s=['python','java',['aps','php'],'scheme']
+len(s)#4
+#get'php' p[1] s[2][1]
+L=[]
+len(l)
+#0
+classmates=('Michael','Bob','Tracy')
+t=(1,2)
+t=()
+t=(1)#misunderstanding
+t=(1,)
+age=3
+if age>=18:
+    print('your name is',age)
+    print('adult')
+elif age>=6:
+    print('teenager')
+else:
+    print('kid')
+birth=input('birth:')
+#input()does not return intefer,returns strings.
+s=input('birth:')
+birth=int(s)
+if birth<2000:
+   print('a')
+else:
+    print('before')
+names=['a','b','c']
+for name in names:
+    print(name)
+sun=0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+    sum=sum+x
+print(sum)
+list((range(5)))#[0,1,2,3,4,5,]
+sum=0
+n=99
+while n>0:
+    sun=sun+n
+    
+
+
+
+
+
+
+
